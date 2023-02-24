6f6bc25525fc574ecda5c99a850cc2b8ed0e43db
NI <75564919+NobuyukiIchioka@users.noreply.github.com>
2023-02-24 17:14:19 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -44,3 +44,37 @@ window.addEventListener('DOMContentLoaded', function(){
   });
   
 });
+
+// 参考コード
+// 'use strict';
+// {
+//   // カバー画面をクリックでクラスを付加
+//   // ついでに動画、動画の枠、音楽のクラスも取ってる
+//   const cover = document.querySelector('.top_click');
+//   const movie = document.querySelector('.movie');
+//   const wrap = document.querySelector('.movie_wrap');
+//   const music = document.querySelector('#music');
+
+//   cover.addEventListener('click', function () {
+//       cover.classList.add('open');
+//       movie.play();
+//   });
+
+//   //動画が再生後に消える
+//   movie.addEventListener('ended', () => {
+//     wrap.classList.add('close');
+//     music.play();
+//   })
+
+//   // 音楽スイッチ
+//   let icon = document.getElementById("icon");
+
+//   icon.onclick = function() {
+//     if(music.paused){
+//       music.play();
+//       icon.src = "./img/music_play.png";
+//     }else{
+//       music.pause();
+//       icon.src = "./img/music_stop.png";
+//     }
+//   }
