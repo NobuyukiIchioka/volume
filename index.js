0dfb6c5adcebe1b6ffcf8251fee56b857805ff3b
NI <75564919+NobuyukiIchioka@users.noreply.github.com>
2023-02-28 22:41:20 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ window.addEventListener('DOMContentLoaded', function(){
 
   const btn_play = document.getElementById("btn_play");
   const btn_pause = document.getElementById("btn_pause");
-  const btn_mute = document.getElementById("btn_mute");
+  const mute_icon = document.getElementById("icon");
   const volume_slider = document.getElementById("volume");
 
   // ボリュームの初期設定
   video.volume = volume_slider.value;
 
-  // 消音ボタンの画像を取得
-   // 音楽スイッチ
-   const icon = document.getElementById("icon");
 
    // 再生ボタンを押したとき
   btn_play.addEventListener("click", e => {
@@ -25,18 +22,31 @@ window.addEventListener('DOMContentLoaded', function(){
   });
 
   // 消音／消音解除ボタンを押したとき
-  btn_mute.addEventListener("click", (e) => {
+  // btn_mute.addEventListener("click", (e) => {
 
-    if( video.muted ) {
+  //   if( video.muted ) {
+  //     video.muted = false;
+  //     btn_mute.textContent = "消音";
+  //     // icon.src = "./img/volume-mute.svg";
+  //   } else {
+  //     video.muted = true;
+  //     btn_mute.textContent = "消音解除";
+  //     // icon.src = "./img/volume-high.svg";
+  //   }
+  // });
+
+ // 消音ボタンの画像を切り替え
+ 
+
+  mute_icon.onclick = function() {
+    if(video.muted){
       video.muted = false;
-      btn_mute.textContent = "消音";
-      icon.src = "./img/volume-mute.svg";
-    } else {
-      video.muted = true;
-      btn_mute.textContent = "消音解除";
       icon.src = "./img/volume-high.svg";
+    }else{
+      video.muted = true;
+      icon.src = "./img/volume-mute.svg";
     }
-  });
+  }
 
   // 音量調整スライダーを操作したとき
   volume_slider.addEventListener("input", (e) => {
